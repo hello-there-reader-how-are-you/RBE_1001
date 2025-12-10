a34416e4164dc0a19bb1763998c7e688ddf67ac8
bella <bellasoutham@gmail.com>
2025-12-10 10:59:10 -0500

    moving some things

--- src/grip.py
+++ src/grip.py
@@ -6,13 +6,15 @@ brain=Brain()
 # Robot configuration code
 arm_motor = Motor(Ports.PORT11, GearSetting.RATIO_18_1, False)
 hand_motor = Motor(Ports.PORT17, GearSetting.RATIO_18_1, False)
-        
+
+def move_arm():
+    arm_motor.spin(FORWARD)
+
 def motorTourqe():
     while hand_motor.torque() < 10:  
         hand_motor.spin(FORWARD)
 
 hand_motor.spin(FORWARD)
 
-def move_arm():
-    if Pick_Fruit: True
-    arm_motor.spin(FORWARD)
+def Deposit_Fruit_In_Basket():
+    hand_motor.spin(REVERSE)
