90e6d1648b3820c0effe9551aec5ddb8cc20867f
Tre-ai <et16code@gmail.com>
2025-12-11 12:50:58 -0500

    Implement Drive_To_Basket function for fruit detection
    Added Drive_To_Basket function to navigate to the detected fruit basket and release the fruit.

--- src/fruit_detect.py
+++ src/fruit_detect.py
@@ -12,3 +12,25 @@ def fruit_detect(fruit):
         CURRENT_FRUIT = "orange"
     else:
         CURRENT_FRUIT = "unknown"
+
+def Drive_To_Basket():
+    global CURRENT_FRUIT
+    basket = detect_basket(CURRENT_FRUIT)
+    if basket:
+        basket_cx = basket.centerX
+        target_cx = X_RESOLUTION/2
+        while abs(basket_cx - target_cx) > 10:
+            basket = detect_basket(CURRENT_FRUIT)
+            if basket:
+                basket_cx = basket.centerX
+                error = basket_cx - target_cx
+                left_motor.spin(FORWARD, clamp(DRIVE_MIN, DRIVE_SPEED - Fruit_PGain*error, DRIVE_MAX))
+                right_motor.spin(FORWARD, clamp(DRIVE_MIN, DRIVE_SPEED + Fruit_PGain*error, DRIVE_MAX))
+        left_motor.stop()
+        right_motor.stop()
+        print("Arrived in front of {CURRENT_FRUIT} basket")
+        # Optionally release fruit
+        hand_motor.spin(REVERSE, 0.5)
+        wait(1, SECONDS)
+        hand_motor.stop()
+        HAVE_FRUIT = False
